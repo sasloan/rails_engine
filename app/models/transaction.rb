6c6c69100fb050470b73077ff5c0a125faec6111
sasloan <sebastiansloan94@gmail.com>
2020-03-18 14:36:36 -0600

    clean up transactions model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,5 +1,5 @@
 class Transaction < ApplicationRecord
-	validates_presence_of :credit_card_number, :credit_card_expiration_date, :result
+	validates_presence_of :credit_card_number, :result
 
-	belongs_to :invoice 
+	belongs_to :invoice
 end
