da6fbf4cc358b00501567e1eee30c553ebada66b
sasloan <sebastiansloan94@gmail.com>
2020-03-18 21:08:05 -0600

    passing spec

--- spec/requests/api/v1/merchant_request_spec.rb
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -21,6 +21,6 @@ describe "Merchant API" do
     merchant = JSON.parse(response.body)
 
     expect(response).to be_successful
-    expect(merchant["id"]).to eq(id)r
+    expect(merchant["id"]).to eq(id)
   end
 end
