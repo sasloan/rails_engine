5205bf306ad30b61733e4eff68bd179200da64ed
sasloan <sebastiansloan94@gmail.com>
2020-03-20 18:15:24 -0600

    Final endpoints created

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 	namespace :api do
     namespace :v1 do
-			get '/api/v1/merchants/:merchant_id/items', to: 'items#index'
-			get '/api/v1/items/:item_id/merchant', to: 'merchants#show'
+			get '/merchants/:merchant_id/items', to: 'items#index'
+			get '/items/:item_id/merchant', to: 'merchants#show'
 
 			resources :merchants, except: [:new, :edit]
 			resources :items, except: [:new, :edit]
